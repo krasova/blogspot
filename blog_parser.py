07df7fdfade6316c9c7061b77ab8a2535cd6a098
kglk029 <olga.ostrovskiy@medimmune.com>
2019-07-17 14:14:46 -0400

    Initial check-in Simple parser for blogspot for title, content and tags

---
+++ blog_parser.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup as Soup
+
+
+def parse_blog(file):
+    handler = open(file, encoding='utf8').read()
+    soup = Soup(handler, 'html5lib')
+    blog = []
+    for entry in soup.findAll('entry'):
+        entry_content = entry.find('content')
+        if entry_content.find('div'):
+            entry_text = ''
+            for div in entry_content.findAll('div'):
+                entry_text += div.getText()
+        else:
+            entry_text = entry_content.getText()
+
+        entry_tags = [category.attrs['term'] for category in entry.findAll('category')]
+        blog_post = {
+            'title': entry.find('title').getText(),
+            'content': entry_text,
+            'tags': entry_tags
+        }
+
+        blog.append(blog_post)
+
+    print(blog[67].get('title'))
+    print(blog[67].get('content'))
+    print(blog[67].get('tags'))
+
+
+if __name__ == "__main__":
+    parse_blog('blog-07-17-2019.xml')
